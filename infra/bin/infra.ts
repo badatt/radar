7a8bf6a41054e4c48bd3d546fc13a44fdc709c77
Balu Praveen Datty <dev.badatt@gmail.com>
2021-05-24 00:26:57 +0530

    added infra

---
+++ infra/bin/infra.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { RadarWebStack } from '../stack/radar-web';
+
+const app = new cdk.App();
+
+const env = {
+  account: app.node.tryGetContext('account'),
+  region: app.node.tryGetContext('region'),
+};
+
+const targetEnv = app.node.tryGetContext('targetEnv');
+
+new RadarWebStack(app, `${targetEnv}-radar-web`, {
+  rootDomain: app.node.tryGetContext('rootDomain'),
+  siteSubdomain: app.node.tryGetContext('siteSubdomain'),
+  stackName: `${targetEnv}-radar-web`,
+  env: env,
+});
+
+app.synth();
